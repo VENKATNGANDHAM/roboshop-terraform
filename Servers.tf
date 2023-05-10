6a1cbc146a202cb925a205f392d028725193caa2
naren <naren.gandham08@gmail.com>
2023-05-10 16:07:53 +0530

    aurgument-output modified

--- Servers.tf
+++ Servers.tf
@@ -1,10 +1,10 @@
-data "aws_ami" "centos"{
-owners  =["973714476881"]
-most_recent =true
-name_regex  ="centos-8-DevOps-practice"
+data "aws_ami"  "centos" {
+owners  = ["973714476881"]
+most_recent = true
+name_regex  = "centos-8-DevOps-practice"
 }
-output "ami"{
-value=data.aws_ami.centos.image_id
+output "ami" {
+value = data.aws_ami.centos.image_id
 }
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
