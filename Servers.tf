c7c952a41c3b70d40f05165ef74cf823a127f17b
naren <naren.gandham08@gmail.com>
2023-05-10 15:42:42 +0530

    aurgument

--- Servers.tf
+++ Servers.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+output "frontend"{
+value =aws_instance.frontend.public_ip
+}
 resource "aws_instance" "catalogue" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
