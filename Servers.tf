5490de4b14ba54cfb15a28b3ab129ec9eaf0934f
naren <naren.gandham08@gmail.com>
2023-05-10 16:10:37 +0530

    aurgument-output ......

--- Servers.tf
+++ Servers.tf
@@ -3,9 +3,7 @@ owners  = ["973714476881"]
 most_recent = true
 name_regex  = "centos-8-DevOps-practice"
 }
-output "ami" {
-value = data.aws_ami.centos.image_id
-}
+
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
